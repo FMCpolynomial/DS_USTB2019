8ec6c3695cf28d61f7fe358ff3ecd9d2f1fc7338
Kesa <kesaubeeire@gmail.com>
2019-03-06 16:47:15 +0800

    暂时完成DS正片，开始搞老师留的副业

--- DS_Program/StackProcess.Designer.cs
+++ DS_Program/StackProcess.Designer.cs
@@ -37,10 +37,10 @@
             this.rT_Expression = new System.Windows.Forms.RichTextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.rT_Result = new System.Windows.Forms.RichTextBox();
+            this.button_Calculate = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.Terminal = new System.Windows.Forms.RichTextBox();
-            this.rT_Result = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -109,7 +109,7 @@
             // 
             this.splitContainer2.Panel2.Controls.Add(this.groupBox3);
             this.splitContainer2.Size = new System.Drawing.Size(705, 1200);
-            this.splitContainer2.SplitterDistance = 383;
+            this.splitContainer2.SplitterDistance = 200;
             this.splitContainer2.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -123,8 +123,8 @@
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 128F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(705, 383);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(705, 200);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // groupBox1
@@ -133,7 +133,7 @@
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(699, 249);
+            this.groupBox1.Size = new System.Drawing.Size(699, 94);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "请输入一个正确的表达式~";
@@ -143,7 +143,7 @@
             this.rT_Expression.Dock = System.Windows.Forms.DockStyle.Fill;
             this.rT_Expression.Location = new System.Drawing.Point(3, 31);
             this.rT_Expression.Name = "rT_Expression";
-            this.rT_Expression.Size = new System.Drawing.Size(693, 215);
+            this.rT_Expression.Size = new System.Drawing.Size(693, 60);
             this.rT_Expression.TabIndex = 0;
             this.rT_Expression.Text = "";
             // 
@@ -151,9 +151,9 @@
             // 
             this.groupBox2.Controls.Add(this.tableLayoutPanel2);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(3, 258);
+            this.groupBox2.Location = new System.Drawing.Point(3, 103);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(699, 122);
+            this.groupBox2.Size = new System.Drawing.Size(699, 94);
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "计算及结果";
@@ -164,24 +164,33 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.05483F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.94517F));
             this.tableLayoutPanel2.Controls.Add(this.rT_Result, 0, 0);
-            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button_Calculate, 0, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 31);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(693, 88);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(693, 60);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
-            // button1
+            // rT_Result
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(3, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(229, 82);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "计算";
-            this.button1.UseVisualStyleBackColor = true;
+            this.rT_Result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rT_Result.Location = new System.Drawing.Point(238, 3);
+            this.rT_Result.Name = "rT_Result";
+            this.rT_Result.Size = new System.Drawing.Size(452, 54);
+            this.rT_Result.TabIndex = 2;
+            this.rT_Result.Text = "";
+            // 
+            // button_Calculate
+            // 
+            this.button_Calculate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_Calculate.Location = new System.Drawing.Point(3, 3);
+            this.button_Calculate.Name = "button_Calculate";
+            this.button_Calculate.Size = new System.Drawing.Size(229, 54);
+            this.button_Calculate.TabIndex = 1;
+            this.button_Calculate.Text = "计算";
+            this.button_Calculate.UseVisualStyleBackColor = true;
             // 
             // groupBox3
             // 
@@ -189,28 +198,20 @@
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox3.Location = new System.Drawing.Point(0, 0);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(705, 813);
+            this.groupBox3.Size = new System.Drawing.Size(705, 996);
             this.groupBox3.TabIndex = 1;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Terminal";
             // 
             // Terminal
             // 
+            this.Terminal.BackColor = System.Drawing.SystemColors.ControlDark;
             this.Terminal.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Terminal.Location = new System.Drawing.Point(3, 31);
             this.Terminal.Name = "Terminal";
-            this.Terminal.Size = new System.Drawing.Size(699, 779);
+            this.Terminal.Size = new System.Drawing.Size(699, 962);
             this.Terminal.TabIndex = 0;
             this.Terminal.Text = "";
-            // 
-            // rT_Result
-            // 
-            this.rT_Result.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rT_Result.Location = new System.Drawing.Point(238, 3);
-            this.rT_Result.Name = "rT_Result";
-            this.rT_Result.Size = new System.Drawing.Size(452, 82);
-            this.rT_Result.TabIndex = 2;
-            this.rT_Result.Text = "";
             // 
             // StackProcess
             // 
@@ -251,7 +252,7 @@
         private System.Windows.Forms.RichTextBox rT_Expression;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button_Calculate;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.RichTextBox Terminal;
         private System.Windows.Forms.RichTextBox rT_Result;
